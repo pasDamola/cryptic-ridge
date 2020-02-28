f3d010b1d6650d4a519c2ae399968013128d68d6
pasDamola <oyindamola.oyetunmibi@outsourceglobal.com>
2020-02-28 03:17:43 -0800

    renamed column names

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -27,9 +27,7 @@ class AccountController < ApplicationController
         user = UsersRecord.where("username = :userName or useremail = :userEmail", { userName: userNameChange.downcase, userEmail: userName }).limit(1)
         if user.count ==1 and user[0].authenticate(password)
             #generate web token
-            # webToken = GenerateLoginToken user[0].userid, user[0].id
-            print (user[0].inspect)
-            print (user[0].userid)
+            webToken = GenerateLoginToken user[0].userid, user[0].id
             # render json: {authentication:webToken, user:user}, status: :ok
             render json: {user:user}, status: :ok
         else

9ad972ab57d8c427e9098a4733b55b21c2252e92
pasDamola <oyindamola.oyetunmibi@outsourceglobal.com>
2020-02-28 03:04:32 -0800

    renamed column names

--- app/models/users_record.rb
+++ app/models/users_record.rb
@@ -5,13 +5,13 @@ class UsersRecord < ApplicationRecord
     has_many :followings
     has_many :followers
     has_secure_password
-    validates :userFullName,:userEmail,:userName, presence: true, on: :create
+    validates :userfullname,:useremail,:username, presence: true, on: :create
     # ^@?(\w){1,15}$
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     #VALID_USER_NAME = \A^@?(\w){1,15}\z/i
-    validates :userEmail, presence: true, length: { maximum: 255 },
+    validates :useremail, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
-    validates_uniqueness_of :userName, uniqueness: { case_sensitive: false }
+    validates_uniqueness_of :username, uniqueness: { case_sensitive: false }
     validates_confirmation_of :password,  on: :create
 end
